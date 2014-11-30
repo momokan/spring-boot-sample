a657fffb4eea32314ccd23cea31b55823361c36d
momokan <momokan@chocolapod.sakura.ne.jp>
2014-11-30 18:08:05 +0900

    外部データベース（非組み込み、非インメモリ）を使う

--- src/main/java/hello/data/entity/Word.java
+++ src/main/java/hello/data/entity/Word.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Entity
+@Entity(name = "words")	//	対応するテーブル名
 public class Word {
 
 	@Id
