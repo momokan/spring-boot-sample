08ffb8628b4deeac967ce3e99760b932bfcc6672
momokan <momokan@chocolapod.sakura.ne.jp>
2014-12-18 12:00:46 +0900

    spring-boot: Spring Security でログインユーザーを取得する

--- spring-boot/src/main/java/hello/controller/PrivateController.java
+++ spring-boot/src/main/java/hello/controller/PrivateController.java
@@ -1,11 +1,10 @@
 package hello.controller;
 
-import hello.form.SigninForm;
-
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PrivateController {
@@ -14,7 +13,12 @@ public class PrivateController {
 	 *	ログイン認証が必要な画面
 	 */
 	@RequestMapping("/private")
-	public String index() {
+	public String index(Model model) {
+		//	ログインユーザーを取得する
+		Authentication	auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		model.addAttribute("username", auth.getName());
+		
 		return "hello/private/index";
 	}
 
