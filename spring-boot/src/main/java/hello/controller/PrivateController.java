602fc7bf40a01f816f62d41451479f9ca7074b91
momokan <momokan@chocolapod.sakura.ne.jp>
2014-12-18 12:15:47 +0900

    spring-boot: セッションを使ってみる

--- spring-boot/src/main/java/hello/controller/PrivateController.java
+++ spring-boot/src/main/java/hello/controller/PrivateController.java
@@ -1,5 +1,7 @@
 package hello.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -8,17 +10,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PrivateController {
+	private static final String		SESSION_KEY_COUNT = "count";
 
 	/**
 	 *	ログイン認証が必要な画面
 	 */
 	@RequestMapping("/private")
-	public String index(Model model) {
+	public String index(HttpSession session, Model model) {
 		//	ログインユーザーを取得する
 		Authentication	auth = SecurityContextHolder.getContext().getAuthentication();
 		
 		model.addAttribute("username", auth.getName());
 		
+		//	セッションにアクセス回数を保持する
+		Integer			count = (Integer)session.getAttribute(SESSION_KEY_COUNT);
+		
+		if (count == null) {
+			count = 0;
+		}
+		count++;
+		
+		session.setAttribute(SESSION_KEY_COUNT, count);
+
+		model.addAttribute("count", count);
+
 		return "hello/private/index";
 	}
 
